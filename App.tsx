87a68a61538671235499f109742d26c3ff40d6b1
paulorofh <paulo.fh@icloud.com>
2023-11-01 16:16:16 -0300

    pastas renomeadas

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
-import { ThemeProvider } from "styled-components/native";
-import theme from "@theme/index";
-import {Groups} from '@screens/groups';
+import { ThemeProvider } from 'styled-components/native';
+
+import theme from './src/theme';
+
+import { Groups } from '@screens/Groups';
 
 export default function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <Groups />
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <Groups />
+        </ThemeProvider>
+    );
 }
